ef913caf50cfbd2bd52b043df968cf382bd3781e
Daria Shutova <dariashutova19@gmail.com>
2023-02-01 23:11:35 +0200

    file cleaning

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,7 @@ function App() {
 	return (
 		<div className="App">
 
-			<Stack direction="horizontal" gap={2}>
-				<Button as="a" variant="primary">
-					Button as link
-				</Button>
-				<Button as="a" variant="success">
-					Button as link
-				</Button>
-			</Stack>
+
 		</div>
 	);
 }
