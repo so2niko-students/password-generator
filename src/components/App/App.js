64b8b355655fae00491eb25fd5ac602dc9038ffb
Yaro <zloyzluka@gmail.com>
2023-02-01 11:11:01 +0200

    init generator progect

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Button, Stack } from 'react-bootstrap';
+function App() {
+	return (
+		<div className="App">
+
+			<Stack direction="horizontal" gap={2}>
+				<Button as="a" variant="primary">
+					Button as link
+				</Button>
+				<Button as="a" variant="success">
+					Button as link
+				</Button>
+			</Stack>
+		</div>
+	);
+}
+
+export default App;
