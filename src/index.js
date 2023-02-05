602c16d3a45e86f034033c24e83d14fc9fadfc80
Daria Shutova <dariashutova19@gmail.com>
2023-02-05 18:32:11 +0200

    register form added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App/App';
 import Test from './components/Test/Test';
+import RegisterForm from './components/App/RegisterForm';
 
 import {
   createBrowserRouter,
@@ -34,6 +35,14 @@ const router = createBrowserRouter([
     element: (
       <div>
         <PasswordsTable/>
+      </div>
+    ),
+  },
+    {
+    path: "/register",
+    element: (
+      <div>
+        <RegisterForm/>
       </div>
     ),
   },
