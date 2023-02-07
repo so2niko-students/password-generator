67f571900ea10b88506e7e13e86859d29478adba
Daria Shutova <dariashutova19@gmail.com>
2023-02-07 21:33:56 +0200

    generation changes

--- src/components/App/PasswordGeneration.js
+++ src/components/App/PasswordGeneration.js
@@ -1,28 +1,53 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from "react";
 
 function PasswordGenerator() {
 
+  const [password, setPassword] = useState('');
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(true);
+  const [numbers, setNumbers] = useState(true);
+  const [symbols, setSymbols] = useState(true);
+  const [passwordLength, setPasswordLength] =useState(8)
+
+
   return (
-    <Form>
+    <Form className="form-container">
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>Password Generator</Form.Label>
+        <Form.Control disabled type="text" placeholder="Generated password" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Label>Password length</Form.Label>
+      <p style={{marginBottom: 0 + 'rem'}}>6</p>
+      <Form.Range min="6" max="30" step="1" />
+      
+      <Form.Group  className="mb-3" controlId="formBasicCheckbox">
+        <Form.Label>Customize your password</Form.Label>
+        <Form.Check checked={symbols} onchange={() => setSymbols (!symbols)} type="checkbox" label=" Include symbols" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Symbols" />
+
+      <Form.Group  className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check checked={numbers} onchange={() => setNumbers (!numbers)} type="checkbox" label="Include numbers" />
       </Form.Group>
+        
+      <Form.Group  className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check checked={lowerCase} onchange={() => setLowerCase (!lowerCase)} type="checkbox" label="Include lowercase letters" />
+      </Form.Group>
+
+      <Form.Group  className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check checked={upperCase} onchange={() => setUpperCase (!upperCase)} type="checkbox" label="Include uppercase letters" />
+      </Form.Group>
+          
       <Button variant="primary" type="submit">
-        Submit
+        Generate Password
+      </Button>{' '}
+
+      <Button variant="success" type="submit">
+        Save
       </Button>
+        
     </Form>
   );
 }
