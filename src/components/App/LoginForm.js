3b6117d633cca8981411fe852c88864c6e187a11
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2023-02-07 18:36:02 +0200

    cors error

--- src/components/App/LoginForm.js
+++ src/components/App/LoginForm.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 
 import PasswordsTable from "./PasswordsTable";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Navigate } from "react-router-dom";
 
 
 
 function LoginForm({Login, error}) {
+
+
 
   const adminUser = {
     email: "admin@admin.com",
@@ -21,6 +21,7 @@ function LoginForm({Login, error}) {
   const [inputError,setError] = useState("");
   const navigate = useNavigate();
 
+
   const LoginInfo = info => {
     console.log(info);
 
@@ -28,51 +29,60 @@ function LoginForm({Login, error}) {
       console.log("Logged in");
       
 
-      /* set route to passwords table, if logged in */
+      /* set route to passwords table if logged in */
+
       navigate("/passwords-table");
 
-      // setUser({
-        
-
-      // })
     } else {
       console.log("Details do not match");
       setError("Details do not match");
-
     }
-
   }
 
-
- 
-
-
-
-    
-  
-
-
-  
-  
    /*local details*/
 
-   const [details, setDetails] = useState({email: "", password: ""});
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+ 
+   useEffect(() => {
+     if (localStorage.getItem("user-info")) {
+       navigate("/add")
+     }
+   })
 
-   const submitHandler = e => {
+  //  const [details, setDetails] = useState({email: "", password: ""});
+
+   async function submitHandler(e) 
+   {
      e.preventDefault();
 
-   LoginInfo(details);
- }
+     console.log(email, password);
 
+     let item = {email, password};
+     let result = await fetch ("https://yarotbot.tk/login", {
+      
+      method: "POST",
+      headers:{
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      },
+      body: JSON.stringify(item)
+      });
+      result = await result.json();
+      localStorage.setItem("user-info", JSON.stringify(result))
+      navigate("/add");
 
-
+  //  LoginInfo(details);
+    }
 
 
   //change auth mode
 
-  let [authMode, setAuthMode] = useState("login");
   const changeAuthMode = () => {
-    setAuthMode(authMode === "login" ? "signup" : "login");
+
+    //! Change route when sign up form available from Dasha
+
+    navigate("/register");
   };
 
   return (
@@ -86,7 +96,7 @@ function LoginForm({Login, error}) {
       </div>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={e =>setDetails({...details, email: e.target.value})} value = {details.email} />
+        <Form.Control type="email" placeholder="Enter email" onChange={e =>setEmail(e.target.value)} />
         <Form.Text className="taext-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -94,13 +104,15 @@ function LoginForm({Login, error}) {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={e =>setDetails({...details, password: e.target.value})} value = {details.password}/>
+        <Form.Control type="password" placeholder="Password" onChange={e =>setPassword(e.target.value)} />
       </Form.Group>
+      <Form.Text className="taext-muted">
+        {(inputError != "") ? (<div className="inputError">{inputError}</div>) : ""}
+      </Form.Text>
       
-      {(inputError != "") ? (<div className="inputError">{inputError}</div>) : ""}
       
       <Button variant="primary" type="submit">
-        Submit
+        Log In
       </Button>
     </Form>
   );
