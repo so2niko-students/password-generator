3686bd2b3f9507c26cca2848051b4ab151717531
Daria Shutova <dariashutova19@gmail.com>
2023-02-09 21:31:13 +0200

    fix on generated pwd + add post new user

--- src/components/App/PasswordGeneration.js
+++ src/components/App/PasswordGeneration.js
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function PasswordGenerator() {
 
@@ -9,42 +10,100 @@ function PasswordGenerator() {
   const [upperCase, setUpperCase] = useState(true);
   const [numbers, setNumbers] = useState(true);
   const [symbols, setSymbols] = useState(true);
-  const [passwordLength, setPasswordLength] =useState(8)
+  const [passwordLength, setPasswordLength] = useState(8);
+  const [selectedChoices, setSelectedChoices] = useState(['lowercase', 'uppercase', 'numbers', 'symbols']);
+  
+  const lowercaseList = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbersList = '0123456789';
+  const symbolsList = "!@#$%^&*()?";
+
+  useEffect(() => {
+    generatePassword()
+  }, [passwordLength]);
+
+  const handleCheckbox = (type) => {
+    let tempChoices = selectedChoices;
+    if(tempChoices.includes(type)){
+        const index = tempChoices.indexOf(type);
+        tempChoices.splice(index,1);
+    }
+    else{
+        tempChoices.push(type);
+    }
+        console.log(tempChoices);
+    setSelectedChoices(tempChoices);
+  }
+  
+  const generatePassword = () => {
+
+    let passwordList = '';
+
+    if (lowerCase) {
+        passwordList += lowercaseList;
+    }
+    if (upperCase) {
+        passwordList += uppercaseList;
+    }
+    if (numbers) {
+        passwordList += numbersList;
+    }
+    if (symbols) {
+        passwordList += symbolsList;
+    }
+
+    let currentPassword = '';
+    const passwordListLength = passwordList.length;
+
+    for (let i = 0; i < passwordLength; i++) {
+      const passwordIndex = Math.floor(Math.random() * passwordListLength);
+      currentPassword += passwordList[passwordIndex];
+    }
+
+    setPassword(currentPassword);
+    
+  }
+
+
+
 
 
   return (
     <Form className="form-container">
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Password Generator</Form.Label>
-        <Form.Control disabled type="text" placeholder="Generated password" />
+        <Form.Control type="text" placeholder="Enter name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control value={password} disabled type="text" placeholder="Generated password" />
       </Form.Group>
 
       <Form.Label>Password length</Form.Label>
-      <p style={{marginBottom: 0 + 'rem'}}>6</p>
-      <Form.Range min="6" max="30" step="1" />
+      <p style={{ marginBottom: 0 + 'rem' }}>{passwordLength}</p>
+      <Form.Range min={6} max={30} step="1" defaultValue={passwordLength} onChange={(event) => setPasswordLength (event.currentTarget.value)} />
       
       <Form.Group  className="mb-3" controlId="formBasicCheckbox">
         <Form.Label>Customize your password</Form.Label>
-        <Form.Check checked={symbols} onchange={() => setSymbols (!symbols)} type="checkbox" label=" Include symbols" />
+        <Form.Check checked={symbols} disabled={selectedChoices.length === 1 && selectedChoices.includes('symbols')} onChange={() => { setSymbols(!symbols); handleCheckbox('symbols');}} type="checkbox" label=" Include symbols" />
       </Form.Group>
 
       <Form.Group  className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check checked={numbers} onchange={() => setNumbers (!numbers)} type="checkbox" label="Include numbers" />
+        <Form.Check checked={numbers} disabled={selectedChoices.length === 1 && selectedChoices.includes('numbers')} onChange={() => { setNumbers(!numbers); handleCheckbox('numbers');}} type="checkbox" label="Include numbers" />
       </Form.Group>
         
       <Form.Group  className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check checked={lowerCase} onchange={() => setLowerCase (!lowerCase)} type="checkbox" label="Include lowercase letters" />
+        <Form.Check checked={lowerCase} disabled={selectedChoices.length === 1 && selectedChoices.includes("lowercase")} onChange={() => { setLowerCase(!lowerCase); handleCheckbox('lowercase');}} type="checkbox" label="Include lowercase letters" />
       </Form.Group>
 
       <Form.Group  className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check checked={upperCase} onchange={() => setUpperCase (!upperCase)} type="checkbox" label="Include uppercase letters" />
+        <Form.Check checked={upperCase} disabled={selectedChoices.length === 1 && selectedChoices.includes('uppercase')} onChange={() => { setUpperCase(!upperCase); handleCheckbox('uppercase');}} type="checkbox" label="Include uppercase letters" />
       </Form.Group>
           
-      <Button variant="primary" type="submit">
+      <Button onClick={generatePassword} variant="primary" type="button">
         Generate Password
       </Button>{' '}
 
-      <Button variant="success" type="submit">
+      <Button variant="success" type="button">
         Save
       </Button>
         
--- src/components/App/RegisterForm.js
+++ src/components/App/RegisterForm.js
@@ -6,37 +6,57 @@ import { useState } from "react";
 import axios from 'axios'
 
 function RegisterForm() {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [repeatedpassword, setrepeatedpassword] = useState()
+  const [errorMessage, seterrorMessage] = useState('')
+  const navigate = useNavigate();
 
-//     const addNewUser = async (newUser) => {
-//   try {
-//     const response = await axios.post('/users', newUser)
-//     return response.data
-//   } catch (err) {
-//     console.error(err.toJSON())
-//   }
-// }
-// addNewUser({ firstName: 'John', lastName: 'Smith' })
+
+
+  function sendNewUser() {
+
+    if (password == repeatedpassword) {
+      console.log(email, password);
+      axios.post('https://yarotbot.tk/register', {
+        email,
+        pwd: password
+      })
+        .then(function (response) {
+          console.log(response);
+          if (response.data.code == 201) {
+            navigate("/login");
+          } else {
+            seterrorMessage('Registation error')
+            setPassword('')
+            setrepeatedpassword('')
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
 
 
   return (
-      <Form className="form-container">
+    <Form className="form-container">
       <h3 className="text-center">Sign up</h3>
+      <h4 className='text-center text-danger'>{errorMessage}</h4>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control value = {email} type="email" placeholder="Enter email" />
+        <Form.Control value={email} onChange={(ev) => setEmail(ev.currentTarget.value)} type="email" placeholder="Enter email" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" >
         <Form.Label>Password</Form.Label>
-        <Form.Control value = {password} type="password" placeholder="Password" />
+        <Form.Control value={password} onChange={(ev) => setPassword(ev.currentTarget.value)} type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3">
         <Form.Label>Repeat Password</Form.Label>
-        <Form.Control type="password" placeholder=" Repeat Password" />
+        <Form.Control value={repeatedpassword} onChange={(ev) => setrepeatedpassword(ev.currentTarget.value)} type="password" placeholder=" Repeat Password" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="button" onClick={sendNewUser}>
         Submit
       </Button>
     </Form>
